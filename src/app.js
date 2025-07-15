c5a277a975e0fe28dc9a6971a0dd595d20e9e28d
Singh <dushyant.singh@emerson.com>
2025-07-16 03:13:13 +0530

    created cloudinary util, all imp setups

--- src/app.js
+++ src/app.js
@@ -25,7 +25,17 @@ app.use(express.urlencoded({ extended: true,
 
 app.use(express.static("public"))
 
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+
+
+// routes
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
 
 export {app}
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary'
+import { response } from 'express';
+import fs from 'fs'
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+    api_key: process.env.CLOUDINARY_API_KEY , 
+    api_secret: process.env.CLOUDINARY_API_SECRET  // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => 
+    {
+        try {
+
+            if(!localFilePath) return null
+
+            const reponse = await cloudinary.uploader.upload(localFilePath, 
+                {resource_type: 'auto'}
+            )
+
+            console.log("File is uploaded on Cloudinary ", response.url);
+            return reponse
+            
+        } catch (error) {
+            fs.unlinkSync(localFilePath)
+            return null 
+        }
+    }
+
+export {uploadOnCloudinary}
