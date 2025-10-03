41f47001363ff39099ce1809a7adc2992acc1c56
Singh <dushyant.singh@emerson.com>
2025-10-04 00:11:40 +0530

    added get channel and subscribed to channels controller and subscription routes

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -60,7 +60,6 @@ const toggleSubscription = asyncHandler(async (req, res) => {
             {
                 isSubscribed,
                 channelId
-
             },
             message
         ))
@@ -69,11 +68,55 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(channelId))
+        {
+            throw new ApiError(400, "Invalid Channel ID")
+        }
+
+    const count = await Subscription.aggregate(
+            {
+                $match : {
+                    channel : mongoose.Types.ObjectId(channelId)
+                }
+
+            },
+
+            {
+                $group : {
+                    _id : null,
+                    totalSubscribers : {$sum : 1}
+                }
+            }
+    )
+
+    const channelCount = count[0] || {totalSubscribers : 0}
+
+    res
+    .status(200)
+    .json( new ApiResponse(200, channelCount, "Subscribed channel(s) fetched successfully "))
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(subscriberId))
+        {
+            throw new ApiError(400, "Invalid User ID")
+        }
+
+    const count = await Subscription.countDocuments(
+            {
+                subscriber : subscriberId
+            }
+    )
+
+    const subscribedToCount = count || 0 
+
+    res
+    .status(200)
+    .json(200, subscribedToCount, "Subscribed channels fetched successfully")
 })
 
 export {
